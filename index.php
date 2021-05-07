b7eedc6263c672ed6047e9bc255a1c29821ff68e
Asulah00 <83156263+Asulah00@users.noreply.github.com>
2021-05-07 10:58:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php
+#
+# Author:   Agent F
+# Name:     Dating Lander Green
+# Version:  1.0
+# Website:  https://afflift.com/
+#
+# START Page variables
+# Offer URL (where you want to send your visitors)
+$offer="https://afflift.com/";
+# Open offer in new tab? (if set to false will open offer in same tab)
+$new_tab=false;
+# Set HTML code to target _blank to open in new tab
+if($new_tab){$open=' target="_blank"';}else{$open="";}
+# Page title
+$title="Online Sex Find";
+#
+# Step 1 text
+$s1title="WARNING! HERE YOU WILL SEE NAKED IMAGES!";
+$s1button="OK";
+#
+# Step 2 text
+$s2title="HERE YOU WILL SEE NAKED IMAGES. WERE DISCOVERED!";
+$s2body="Before we show you a list and pictures of women who live in your area and who are ready to have sex right now, we need to ask you 3 questions.";
+$s2button="Continue »";
+#
+# Questions are steps 3 to 5
+# Question common text (used in all 3 questions)
+$qtitle="Question";
+$qn1="1";
+$qn2="2";
+$qn3="3";
+$no="No";
+$yes="Yes";
+#
+# Questions
+$question1="Several of these women are young mothers who are single or married and interested in freeing themselves. They can be neighbors to you or someone you already know. Do you agree to keep these women's identity secret?";
+$question2="These women asked us not to let men interested in 'relationships' participate. They have made it clear that they only want quick, straightforward sex. They are not interested in anything serious. Do you agree with their wishes?";
+$question3="Do you agree to use a condom when you have sex with someone you know through our site?";
+#
+# Step 6 Final Page
+$s6title="Thanks!";
+$s6body="Now you can look at our list and our pictures of women from your region.<br><br>Click on the 'Continue' button and find women who want to fuck.";
+$s6button="Continue »";
+#
+# Back button offer URL (can be same as the offer or something else up to you!)
+$back="https://afflift.com/f/members/agentf.6188/#recent-content";
+# END Page variables
+
+?>
+<!-- START html -->
+<!DOCTYPE html>
+<html lang="en-US" class="gr__galaxy-love_com">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="noindex, nofollow, nosnippet">
+    <title><?= $title ?></title>
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <style>.top{width:100%;font-size:29px;color:#14990a;font-family:'Open Sans',sans-serif;font-weight:700;padding-top:12px;padding-bottom:12px}.box{margin:0 auto;padding:10px}#q0,#q00,#q1,#q2,#q3,#q4,#q5,#q6,#q7,#q8,#q9{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);overflow:hidden;padding:0;width:100%;min-width:300px;max-width:600px;display:none;line-height:28px;font-size:22px;color:#000;font-family:'Open Sans',sans-serif}div{display:block}.h1{font-size:40px;color:#000;font-family:'Open Sans',sans-serif;line-height:1;font-weight:700}.intro-image{width:50%}.border10{border-radius:10px}.float-left{float:left}#container{padding:0;padding-top:0;width:95%;min-width:300px;max-width:600px;position:relative;margin:10px auto}.circle1{font-size:23px;color:#fff;font-family:'Open Sans',sans-serif;font-weight:700;background:#14990a;border-radius:3px;padding:8px 18px;margin-left:3px}.circle2{font-size:23px;color:#888;font-family:'Open Sans',sans-serif;font-weight:700;background:#ececec;border-radius:3px;padding:8px 18px;margin-left:3px}.btnno{font-size:30px;color:#fff;font-family:'Open Sans',sans-serif;width:100%;min-width:200px;background-color:#aaa;border-radius:4px;display:inline-block;cursor:pointer;font-weight:700;padding:20px 0;text-decoration:none;margin:10px 0 0 0}.btnyes{font-size:30px;color:#fff;font-family:'Open Sans',sans-serif;width:100%;min-width:200px;background-color:#14990a;border-radius:4px;display:inline-block;cursor:pointer;font-weight:700;padding:20px 0;text-decoration:none;margin:10px 0 0 0}@media screen and (min-width:1200px){.intro-image{width:40%}}@media screen and (max-width:500px){#q0,#q00,#q1,#q2,#q3,#q4,#q5,#q6,#q7,#q8,#q9{font-size:18px;line-height:21px}.h1{font-size:22px}.btnyes{font-size:24px;width:100%}.top{font-size:16px}}.center{text-align:center}.left{text-align:left}.top{width:100%;font-size:29px;color:#14990a;font-family:'Open Sans',sans-serif;font-weight:700;padding-top:12px;padding-bottom:12px}.box{margin:0 auto;padding:10px}#q0,#q00,#q1,#q2,#q3,#q4,#q5,#q6,#q7,#q8,#q9{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);overflow:hidden;padding:0;width:100%;min-width:300px;max-width:600px;display:none;line-height:28px;font-size:22px;color:#000;font-family:'Open Sans',sans-serif}div{display:block}.h1{font-size:40px;color:#000;font-family:'Open Sans',sans-serif;line-height:1;font-weight:700}.intro-image{width:50%}.border10{border-radius:10px}.float-left{float:left}#container{border-radius:0;padding:0;padding-top:0;width:95%;min-width:300px;max-width:600px;position:relative;margin:10px auto}.circle1{font-size:23px;color:#fff;font-family:'Open Sans',sans-serif;font-weight:700;background:#14990a;border-radius:3px;padding:8px 18px;margin-left:3px}.circle2{font-size:23px;color:#888;font-family:'Open Sans',sans-serif;font-weight:700;background:#ececec;border-radius:3px;padding:8px 18px;margin-left:3px}.btnno{font-size:30px;color:#fff;font-family:'Open Sans',sans-serif;width:100%;min-width:200px;background-color:#aaa;border-radius:4px;display:inline-block;cursor:pointer;font-weight:700;padding:20px 0;text-decoration:none;margin:10px 0 0 0}.btnyes{font-size:30px;color:#fff;font-family:'Open Sans',sans-serif;width:100%;min-width:200px;background-color:#14990a;border-radius:4px;display:inline-block;cursor:pointer;font-weight:700;padding:20px 0;text-decoration:none;margin:10px 0 0 0}@media screen and (min-width:1200px){.intro-image{width:40%}}@media screen and (max-width:500px){#q0,#q00,#q1,#q2,#q3,#q4,#q5,#q6,#q7,#q8,#q9{font-size:18px;line-height:21px}.h1{font-size:22px}.btnyes{font-size:24px;width:100%}.top{font-size:16px}}</style>
+</head>
+<body>
+    <div>
+        <div id="container" class="left">
+            <div style="height:100vh;position:relative">
+                <!-- Start Step One -->
+                <div id="q00" class="toggleDiv center" style="display: block;">
+                    <div class="center top"><span class="h1"><?= $s1title ?></span></div>
+                    <div class="center box">
+                        <img src="img/jump_zon_03.png" class="intro-image border10" alt="intro image" >
+                        <img src="img/jump_zon_01.png" class="float-left intro-image" alt="intro image">
+                        <img src="img/jump_zon_04.png" class="intro-image border10" alt="intro image" >
+                        <img src="img/jump_zon_02.png" class="float-left intro-image" alt="intro image">
+                        <div>
+                            <a class="show_hide btnyes" href="javascript:void(0)" rel="#q0"><?= $s1button ?></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Step One -->
+                <!-- Start Step Two -->
+                <div id="q0" class="toggleDiv left" style="display: none;">
+                    <div class="center top"><span class="h1"><?= $s2title ?></span></div>
+                    <div class="box"><?= $s2body ?></div>
+                    <div class="center">
+                        <div><a class="show_hide btnyes" href="javascript:void(0)" rel="#q1"><?= $s2button ?></a></div>
+                    </div>
+                </div>
+                <!-- End Step Two -->
+                <!-- Start Step Three -->
+                <div id="q1" class="toggleDiv left" style="display: none;">
+                    <div class="center top"><?= $qtitle ?>
+                        <span class="circle1"><?= $qn1 ?></span>
+                        <span class="circle2"><?= $qn2 ?></span>
+                        <span class="circle2"><?= $qn3 ?></span>
+                    </div>
+                    <div class="box"><?= $question1 ?></div>
+                    <div class="center">
+                        <div><a class="show_hide btnyes" href="javascript:void(0)" rel="#q2"><?= $yes ?></a></div>
+                        <div><a class="show_hide btnno" href="javascript:void(0)" rel="#q2"><?= $no ?></a></div>
+                    </div>
+                </div>
+                <!-- End Step Three -->
+                <!-- Start Step Four -->
+                <div id="q2" class="toggleDiv left" style="display: none;">
+                    <div class="center top"><?= $qtitle ?>
+                        <span class="circle2"><?= $qn1 ?></span>
+                        <span class="circle1"><?= $qn2 ?></span>
+                        <span class="circle2"><?= $qn3 ?></span>
+                    </div>
+                    <div class="box"><?= $question2 ?></div>
+                    <div class="center">
+                        <div><a class="show_hide btnyes" href="javascript:void(0)" rel="#q3"><?= $yes ?></a></div>
+                        <div><a class="show_hide btnno" href="javascript:void(0)" rel="#q3"><?= $no ?></a></div>
+                    </div>
+                </div>
+                <!-- End Step Four -->
+                <!-- Start Step Five -->
+                <div id="q3" class="toggleDiv left" style="display: none;">
+                    <div class="center top"><?= $qtitle ?>
+                        <span class="circle2"><?= $qn1 ?></span>
+                        <span class="circle2"><?= $qn2 ?></span>
+                        <span class="circle1"><?= $qn3 ?></span>
+                    </div>
+                    <div class="box"><?= $question3 ?></div>
+                    <div class="center">
+                        <div><a class="show_hide btnyes" href="javascript:void(0)" rel="#q9"><?= $yes ?></a></div>
+                        <div><a class="show_hide btnno" href="javascript:void(0)" rel="#q9"><?= $no ?></a></div>
+                    </div>
+                </div>
+                <!-- End Step Five -->
+                <!-- Start Step Six -->
+                <div id="q9" class="toggleDiv left" style="display: none;">
+                    <div class="center top"><?= $s6title ?></div>
+                    <div class="box"><?= $s6body ?></div>
+                    <div class="center"><a id="q5s" class="btnyes" href="<?= $offer ?>"><?= $s6button ?></a></div>
+                </div>
+                <!-- End Step Six -->
+            </div>
+        </div>
+    </div>
+    <!-- Start Jquery CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <!-- End Jquery CDN -->
+    <!-- Start Javascript functions to hide/show questions -->
+    <script>!function(n){n.fn.showHide=function(i){i=n.extend({speed:1e3,easing:"",changeText:1,showText:"Show",hideText:"Hide"},i);n(this).click(function(){n(".toggleDiv").fadeOut(i.speed,i.easing);var e=n(this),t=n(this).attr("rel");return n(t).fadeIn(i.speed,i.easing,function(){1==i.changeText&&(n(t).is(":visible")?e.text(i.hideText):e.text(i.showText))}),!1})}}(jQuery),$(document).ready(function(){$(".show_hide").showHide({speed:400,easing:"",changeText:1,showText:"View",hideText:""})}),$(window).load(function(){setTimeout(function(){$("html, body").animate({scrollTop:$(document).height()},1700)},1500)});</script>
+    <!-- End Javascript functions to hide/show questions -->
+    <!-- START Back button redirect script -->
+    <script>!function(){var o;try{for(URL=window.location.href.split(/[#]/)[0],o=0;o<10;++o)history.pushState({},"",URL+"#");onpopstate=function(o){o.state&&location.replace("<?= $back;?>")}}catch(o){console.log(o)}}();</script>
+    <!-- END Back button redirect script -->
+    </body>
+</html>
+<!-- END HTML -->
